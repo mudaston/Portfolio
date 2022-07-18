eb3dfc0178bed77bf7d4060387e3cb136639b04f
Mudastone <twil.revaas@gmail.com>
2022-07-18 22:29:44 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    function closeMenu() {
+        menu.classList.remove('active')
+        menuBlock.classList.remove('active')
+        overlap.classList.remove('active')
+        document.body.removeAttribute('style')
+    }
+
+    function openMenu() {
+        const paddingRight = scrollbarWidth()
+
+        menu.classList.add('active')
+        menuBlock.classList.add('active')
+        overlap.classList.add('active')
+        document.body.style.overflow     = 'hidden'
+        document.body.style.paddingRight = `${paddingRight}px`
+    }
+
+    function scrollbarWidth() {
+        const documentWidth = parseInt(document.documentElement.clientWidth);
+        const windowsWidth  = parseInt(window.innerWidth);
+        return windowsWidth - documentWidth;
+    }
+
+    const closer    = document.querySelector('.menu__close'),
+          burger    = document.querySelector('.menu__burger'),
+          overlap   = document.querySelector('.menu__overlap'),
+          menuBlock = document.querySelector('.menu__block'),
+          menu      = document.querySelector('.menu')
+
+    burger.addEventListener('click', () => openMenu())
+
+    overlap.addEventListener('click', () => closeMenu())
+
+    closer.addEventListener('click', () => closeMenu())
+
+    menuBlock.addEventListener('click', (e) => {
+        const target = e.target
+
+        if (target && target.matches('.menu__link') || target.matches('.menu__social-link')) closeMenu()
+    })
+
+})
---
+++ js/theme.js
@@ -0,0 +1,64 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkMode          = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    const html              = document.querySelector('html')
+    const changeThemeButton = document.querySelector('.menu__change-theme')
+    const htmlIconText      = document.querySelector('.html-icon').querySelectorAll('.text')
+    const cssIconText       = document.querySelector('.css-icon-text')
+    const nodeJsText        = document.querySelector('.node-js-icon').querySelectorAll('.text')
+
+    const mutationObserver = new MutationObserver((entries) => {
+        const darkMode = entries[0].target.dataset.theme === 'dark'
+        const color    = darkMode ? '#fff' : '#000'
+
+        htmlIconText.forEach((item) => {
+            item.setAttribute('fill', color)
+        })
+        nodeJsText.forEach((item) => {
+            item.setAttribute('fill', color)
+        })
+        cssIconText.setAttribute('fill', color)
+    })
+
+    mutationObserver.observe(html, {attributes: true})
+
+    const changeTheme = () => {
+        let currentTheme = html.dataset.theme
+
+        switch (currentTheme) {
+            case 'dark':
+                currentTheme = 'light'
+                break
+            case 'light':
+                currentTheme = 'dark'
+                break;
+            default:
+                currentTheme = `${darkMode ? 'dark' : 'light'}`
+        }
+
+        html.dataset.theme = currentTheme
+
+        localStorage.setItem('theme', currentTheme)
+    }
+
+    const changeButtonThemeIcon = () => {
+        html.dataset.theme === 'dark' ?
+            changeThemeButton.classList.remove('icon-sun') :
+            changeThemeButton.classList.add('icon-sun')
+    }
+
+    changeThemeButton.addEventListener('click', () => {
+        changeTheme()
+        changeButtonThemeIcon()
+    })
+
+    const savedLanguage = localStorage.getItem('theme')
+
+    if (savedLanguage)
+        html.dataset.theme = savedLanguage
+    else
+        changeTheme()
+
+    changeButtonThemeIcon()
+})
